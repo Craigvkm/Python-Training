9a19341219c491e65371ca811982cd306f85790c
Craig M <craigvkm@gmail.com>
2015-07-07 16:43:12 -0400

    Revert "Silly Python Works"
    This reverts commit 3183484ba7ecc451ca067f078e3f72e9d1f4d93f.

--- import.py
+++
@@ -1,9 +0,0 @@
-import math
-
-print(math.sqrt(25))
-print(dir(math)) #shows all functions available from math
-
-
-from math import sqrt
-
-print(sqrt(9))
--- main.py
+++
@@ -1,7 +0,0 @@
-#from functions import * # Imports all of my made functions
-						# doesnt require functions.func() due to importing all
-
-all_functions = dir()
-print(all_functions)
-
-import 
